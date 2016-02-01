05416d217bf5e1cd19ee3871bf71b39508a93696
Ilnur Sayfutdinov <ilnursoft@gmail.com>
2016-02-01 21:48:50 +0300

    add listurl

--- go-web-crawler.go
+++ go-web-crawler.go
@@ -24,7 +24,7 @@ import (
 //---- инициализация глобальных типов и переменных
 type ListUrl struct {
 	url       string // урл
-	fdownload bool   // флаг того что данный урл был загружен для анализа
+	fdownload int    // флаг того что данный урл был загружен для анализа: if 1 then загружен ; if 0 не загружен; if -1 ошибка при загрузке
 }
 
 var (
@@ -46,25 +46,28 @@ func InitLogFile(namef string) *log.Logger {
 }
 
 //получение страницы из урла url
-func gethtmlpage(url string) []byte {
+func gethtmlpage(url string) ([]byte, bool) {
 	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Println("HTTP error:", err)
-		panic("HTTP error")
+		//		panic("HTTP error")
+		return make([]byte, 0), false
 	}
 	defer resp.Body.Close()
 	// вот здесь и начинается самое интересное
 	utf8, err := charset.NewReader(resp.Body, resp.Header.Get("Content-Type"))
 	if err != nil {
 		fmt.Println("Encoding error:", err)
-		panic("Encoding error")
+		return make([]byte, 0), false
+		//		panic("Encoding error")
 	}
 	body, err := ioutil.ReadAll(utf8)
 	if err != nil {
 		fmt.Println("IO error:", err)
-		panic("IO error")
+		return make([]byte, 0), false
+		//		panic("IO error")
 	}
-	return body
+	return body, true
 }
 
 //сохранить данные в файл
@@ -113,10 +116,10 @@ func internalLinksfromSiteListUrl(ll []ListUrl, dom string) []ListUrl {
 	for _, v := range ll {
 		if len(v.url) > 0 {
 			if v.url[0] == '/' {
-				res = append(res, ListUrl{url: dom + v.url, fdownload: false})
+				res = append(res, ListUrl{url: dom + v.url, fdownload: 0})
 			} else {
 				if strings.HasPrefix(v.url, dom) {
-					res = append(res, ListUrl{url: v.url, fdownload: false})
+					res = append(res, ListUrl{url: v.url, fdownload: 0})
 				}
 			}
 		}
@@ -151,7 +154,7 @@ func UniqLinks2(list1 []ListUrl, list2 []ListUrl) []ListUrl {
 	for _, v2 := range list2 {
 		f := false
 		for _, v1 := range list1 {
-			if (v1.url == v2.url) && (v1.fdownload == v2.fdownload) { //strings.Compare(v1, v2) == 0 {
+			if v1.url == v2.url { // && (v1.fdownload == v2.fdownload) { //strings.Compare(v1, v2) == 0 {
 				f = true
 				break
 			}
@@ -200,6 +203,26 @@ func delPovtor(l []string) []string {
 	return res
 }
 
+//удаление повторов в массиве
+func delPovtor2(l []ListUrl) []ListUrl {
+	var f bool
+	res := make([]ListUrl, 0)
+	for i := 0; i < len(l); i++ {
+		f = true
+		for j := 0; j < i; j++ {
+			if l[i].url == l[j].url { // && (l[i].fdownload == l[j].fdownload) {
+				f = false
+				break
+			}
+		}
+		if f {
+			res = append(res, l[i])
+			f = true
+		}
+	}
+	return res
+}
+
 //-----------------
 // функция парсинга аргументов программы
 func parse_args() bool {
@@ -210,7 +233,7 @@ func parse_args() bool {
 		site = "http://echo.msk.ru"
 	}
 	if koliter == "" {
-		koliter = "100"
+		koliter = "10"
 	}
 	return true
 }
@@ -229,32 +252,42 @@ func main() {
 	timestart := time.Now().String()
 	//	flagEnd := false // флаг окончания выгрузки
 	lurl := make([]ListUrl, 0) // make([]string, 0)
-	lurl = append(lurl, ListUrl{url: myurl, fdownload: false})
+	lurl = append(lurl, ListUrl{url: myurl, fdownload: 0})
 	c := 0
 	for {
 		if (c == ckoliter) || (c > len(lurl)-1) {
 			break
 		} else {
 			fmt.Print("c= ", c)
-			body := gethtmlpage(lurl[c].url)
-			listlinks := getLnksfromPage(body)
-			//			fmt.Println(listlinks)
-			listnew := internalLinksfromSite(listlinks, myurl)
-			listnew2 := UniqLinks2(lurl, listnew)
-			lurl = AddtoEndList2(lurl, listnew2)
-			fmt.Println("   len(lurl)= ", len(lurl))
+			body, errs := gethtmlpage(lurl[c].url)
+			if errs {
+				lurl[c].fdownload = 1 // обработан url
+				listlinks := getLnksfromPage(body)
+				listlinksurl := make([]ListUrl, 0)
+				for _, vv := range listlinks {
+					listlinksurl = append(listlinksurl, ListUrl{url: vv, fdownload: 0})
+				}
+				//			fmt.Println(listlinks)
+				listnew := internalLinksfromSiteListUrl(listlinksurl, myurl)
+				//				listnew2 := UniqLinks2(lurl, listnew)
+				lurl = AddtoEndList2(lurl, listnew)
+				lurl = delPovtor2(lurl)
+				fmt.Println("   len(lurl)= ", len(lurl))
+			} else {
+				lurl[c].fdownload = -1 // обработан url была ошибка
+			}
 			c++
 		}
 	}
 	fmt.Println("c= ", c)
 	fmt.Println("len(lurl)= ", len(lurl))
 
-	lurl = delPovtor(lurl)
+	lurl = delPovtor2(lurl)
 	fmt.Println("после удаления дубликатов - len(lurl)= ", len(lurl))
 
 	s := ""
 	for _, v := range lurl {
-		s += v + "\n"
+		s += v.url + ";" + strconv.Itoa(v.fdownload) + "\n"
 	}
 	timeend := time.Now().String()
 	fmt.Println("Time Start...", timestart)
